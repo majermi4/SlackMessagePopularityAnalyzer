91f23267ad71f3b65ed003202523fb7973d19010
Michal Majer <michal.majer@rohea.com>
2020-01-04 17:35:33 +0100

    Improve Message model class.

--- src/majermi4/MessagePopularityAnalyzer/Model/Message.kt
+++ src/majermi4/MessagePopularityAnalyzer/Model/Message.kt
@@ -1,24 +1,34 @@
 package majermi4.MessagePopularityAnalyzer.Model
 
+import majermi4.MessagePopularityAnalyzer.SlackChannel
 import java.io.Serializable
 
 data class Message(
+    val timestamp: String, // Slack message ID
     val text: String,
     val reactions: List<Reaction>,
     val authorId: String?,
     val replyCount : Int,
-    val replyUsersCount: Int
+    val replyUsersCount: Int,
+    val channel: SlackChannel
 
 ) : Serializable
 {
     fun totalReactionCount() : Int
     {
-        var totalReactionCount = 0
+        return reactions
+            .fold(0, { totalEmojiCount, reaction -> totalEmojiCount + reaction.count })
+    }
 
-        for (reaction in reactions) {
-            totalReactionCount += reaction.count
-        }
+    fun emojiCount(emojiName: String) : Int
+    {
+        return reactions
+            .filter { it.name.contains(emojiName) }
+            .fold(0, { totalEmojiCount, reaction -> totalEmojiCount + reaction.count })
+    }
 
-        return totalReactionCount
+    fun getUrlLink() : String
+    {
+        return "https://rohea.slack.com/archives/${channel.id}/p${timestamp.replace(".", "")}"
     }
 }
