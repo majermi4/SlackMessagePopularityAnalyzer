bb4f1ce30e20f70bcca9ed3395911b55ae6723a9
Michal Majer <michal.majer@rohea.com>
2020-01-04 17:40:46 +0100

    Read auth token from a file, add more verbose output to main method.

--- src/majermi4/MessagePopularityAnalyzer/Main.kt
+++ src/majermi4/MessagePopularityAnalyzer/Main.kt
@@ -1,18 +1,44 @@
 package majermi4.MessagePopularityAnalyzer
 
+import java.io.File
+
 fun main()
 {
-    val authToken = "XXX"
-    val channel = SlackChannel.RANDOM
+    val authToken = File("authToken").readText()
+
+    val startTime: Long = System.currentTimeMillis()
 
     val messageParser = MessageParser(authToken)
-    val messages = messageParser.execute(channel)
+    val userParser = UserParser(authToken)
 
-    val popularMessages = MessageAnalyzer.findPopular(messages, 6)
+    val messages = messageParser.parseMessagesFromAllChannels()
 
-    println("Done. Analyzed ${messages.count()} messages in #${channel.getName()} channel. Found ${popularMessages.count()} popular messages.")
+    messages.sortByGreatestUseOfEmoji("+1")
+    //messages.sortByGreatestUseOfEmoji("-1")
+    //messages.sortByTextLength();
+    // TODO: Sort by fastest replies
 
-    popularMessages.forEachIndexed { idx, popularMessage ->
-        println("Popular message #$idx text: ${popularMessage.text}")
+    val endTime: Long = System.currentTimeMillis()
+
+    val timeElapesed = (endTime - startTime)
+
+    println("Done. Analyzed ${messages.count()} messages in $timeElapesed ms.")
+
+    messages.subList(0, 5).forEach { popularMessage ->
+        var authorId = "Unknown"
+        var authorName = "Unknown"
+
+        if (popularMessage.authorId != null) {
+            val author = userParser.parseUser(popularMessage.authorId)
+            authorId = author.userId
+            authorName = author.userName
+        }
+
+        println("-----------------------------------------------------------")
+        println("Message id: ${popularMessage.timestamp}")
+        println("Message text: ${popularMessage.text}")
+        println("User: $authorName (Id: $authorId)")
+        println("Channel: ${popularMessage.channel.getName()}")
+        println("Link: ${popularMessage.getUrlLink()}")
     }
 }
